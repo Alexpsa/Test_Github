0daae162f836356e3e2704a0054603b7337cb08b
Alexpsa <psathasalex@yahoo.com>
2015-04-22 11:15:36 +0300

    Initial Commit

---
+++ Test_Github/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: delta
+ * Date: 22/4/2015
+ * Time: 11:13 πμ
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test_Github
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
